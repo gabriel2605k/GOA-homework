4a9abd26a86f84bb5fe11f3fbce703b7e73942d7
gabikarkashadze@gmail.com <gabikarkashadze@gmail.com>
2024-07-23 17:19:42 +0400

    commit

---
+++ lvl 2/404 2.py
@@ -0,0 +1,5 @@
+name="gabi"
+username="karkashadze"
+age=12
+city="kutaisi"
+print(name +" "+ username +" is my name"+  str(age) +"years from "+ city)
---
+++ lvl 2/404.py
@@ -0,0 +1,10 @@
+name=("gabi")
+print("hello gabi")
+
+name="gabi"
+username="karkashadze"
+age="12"
+city="kutaisi"
+
+print=(name +" "+ username +" "+ age +" "+ city)
+
