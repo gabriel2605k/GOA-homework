71487db3d53fd7a15c05e2f1bdb37b677090a5fb
garbiel2605k <gabikarkashadze@gmail.com>
2024-10-14 13:38:55 +0400

    second commit

--- levle 32/homework/hw.py
+++ levle 32/homework/hw.py
@@ -1,7 +1,7 @@
 # 1) შექმენით რიცხვებით სავსე სია, შემდეგ დაწერეთ კოდი რომელიც დაბეჭდავს ამ 
 # სიიდან ყველაზე უდიდეს რიცხვს გამოგადგებათ for loop ი კარგად დაფიქრდით და გაიაზრეთ
 numbers = [1,2,3,4,5,62,2,1,4,232,62,346,43,6346,3436,634633643634]
-largest_number = numbers[0]  #
+largest_number = numbers[0]  
 for num in numbers:
     if num > largest_number:
         largest_number = num
