603cacc7e8c1b69298dcceb3e041155fc1b42b03
garbiel2605k <gabikarkashadze@gmail.com>
2024-11-15 20:47:12 +0400

    second commit

--- level 38/homework/hw2.py
+++ level 38/homework/hw2.py
@@ -1,13 +1,13 @@
 # 2) შექმენით ფუნქცია რომელსაც არგუმენტად გადაეცემა რიცხვებით 
 # სავსე სია შემდეგ კი ამ ფუნქციამ უნდა დააბრუნოს უდიდესი რიცხვი ამ სიიდან
-def უდიდესი_რიცხვის_მოძებნა(რიცხვები):
-    if not რიცხვები: 
+def search_biggest(nums):
+    if not nums: 
         return None 
-    უდიდესი = რიცხვები[0]  
-    for რიცხვი in რიცხვები:
-        if რიცხვი > უდიდესი:
-            უდიდესი = რიცხვი 
-    return უდიდესი 
+    biggest = num[0]  
+    for num in nums:
+        if num > biggest:
+            biggest = num
+    return biggest
 
 სია = [3, 5, 1, 8, 2]
-print(უდიდესი_რიცხვის_მოძებნა(სია))  
+print(search_biggest(list))  
